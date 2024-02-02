81a9a019c44911b16eb1716533f68e4563892967
Andrey Pereira <andreysp@dcc.ufrj.br>
2024-02-02 10:33:38 -0300

    Adjusting authorization - photo#destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,9 +2,8 @@ class PhotosController < ApplicationController
   def destroy
     @flat = Flat.find(params[:flat_id])
     @photo = @flat.photos.find_by(id: params[:id])
+    authorize @photo, policy_class: PhotoPolicy
     @photo.purge
-    # @flat.update
-    # authorize @photo
     redirect_to edit_flat_path(@flat)
   end
 end
